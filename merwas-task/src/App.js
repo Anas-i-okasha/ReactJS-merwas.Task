d95c00d3b62fbf84ec717335027f21918d37daee
Anas-i-okasha <okasha.i.anas@gmail.com>
2021-04-09 19:43:53 +0300

    start the task frontend only

---
+++ merwas-task/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Electrical Engineering</h1>
+      <h2>Full-stack Developer</h2>
+      <div>Mutah university</div>
+     
+    </div>
+  );
+}
+
+export default App;
