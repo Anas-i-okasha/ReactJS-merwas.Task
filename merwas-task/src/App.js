71543b9e5df80f91e09dfe71fe12188eb64cb15c
Anas-i-okasha <okasha.i.anas@gmail.com>
2021-04-10 16:00:26 +0300

    part two is finished

--- merwas-task/src/App.js
+++ merwas-task/src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar';
 import Main from './Components/main/Main';
 import Image from './Components/image/image';
-import Sidebar from './Components/SideBar/Side'
+import Sidebar from './Components/SideBar/Side';
+import PartTwo from './Components/PartTwo/partTwo'
 import './App.css';
 
 const App=()=> {
@@ -12,6 +13,7 @@ const App=()=> {
       <Main/>
       <Image/>
       <Sidebar/>
+      <PartTwo/>
     </div>
   );
 }
---
+++ merwas-task/src/Components/PartTwo/partTwo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './partTwo.css'
+
+ const partTwo=()=> {
+    return (
+        <div className='two'>
+            <div className='part-two'>
+              <h4 style={{color:'blue'}}>Get the best deals delivered direct to your inbox each day</h4> <input style={{width:'30%', height:'7vh' , marginRight:-120}} className='form-control ' placeholder='Enter Email Address' /><button className='btn btn-primary btn-lg'>Subscrib</button>   
+            </div>
+        </div>
+    )
+}
+export default partTwo
